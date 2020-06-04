5e42a6f9bf16c20f8638fbbd174bbfa17cc048b8
kwu3 <kwu3@villanova.edu>
2020-06-03 22:11:26 -0400

    Created nav tabs for each page

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
 import React from "react";
+import NavTabs from "./components/NavTabs";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <NavTabs />
+    </div>
+  );
 }
 
 export default App;
