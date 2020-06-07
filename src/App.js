c14dcc961c362acaf7b9b2f58628b295d54c67c4
kwu3 <kwu3@villanova.edu>
2020-06-06 20:44:18 -0400

    Added react-router-dom to handle page render

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
 import React from "react";
 import NavTabs from "./components/NavTabs";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
+import About from "./pages/About";
 
 function App() {
   return (
-    <div className="App">
-      <NavTabs />
-    </div>
+    <BrowserRouter>
+      <div className="App">
+        <NavTabs />
+        <Switch>
+          <Route exact path="/" component={About} />
+          <Route exact path="/about" component={About} />
+        </Switch>
+      </div>
+    </BrowserRouter>
   );
 }
 
