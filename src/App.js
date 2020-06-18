e168d470b0ea0cd1d10f476be57ba06be89d4da4
kwu3 <kwu3@villanova.edu>
2020-06-17 21:07:13 -0400

    Added routes to other pages

--- src/App.js
+++ src/App.js
@@ -1,16 +1,21 @@
-import React from "react";
-import NavTabs from "./components/NavTabs";
-import { BrowserRouter, Route, Switch } from "react-router-dom";
-import About from "./pages/About";
+import React from 'react';
+import NavTabs from './components/NavTabs';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import Landing from './pages/Landing';
+import About from './pages/About';
+import Contact from './pages/Contact';
+import Projects from './pages/Projects';
 
 function App() {
   return (
     <BrowserRouter>
-      <div className="App">
+      <div className='App'>
         <NavTabs />
         <Switch>
-          <Route exact path="/" component={About} />
-          <Route exact path="/about" component={About} />
+          <Route exact path='/' component={Landing} />
+          <Route exact path='/about' component={About} />
+          <Route exact path='/contact' component={Contact} />
+          <Route exact path='/projects' component={Projects} />
         </Switch>
       </div>
     </BrowserRouter>
