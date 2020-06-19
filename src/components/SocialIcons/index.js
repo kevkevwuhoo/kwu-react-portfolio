372f987de341b26cfbc84aa47f2e7bac18376710
kwu3 <kwu3@villanova.edu>
2020-06-19 15:58:25 -0400

    Added styles to icon buttons

--- src/components/SocialIcons/index.js
+++ src/components/SocialIcons/index.js
@@ -3,18 +3,19 @@ import IconButton from '@material-ui/core/IconButton';
 import GitHubIcon from '@material-ui/icons/GitHub';
 import LinkedInIcon from '@material-ui/icons/LinkedIn';
 import EmailIcon from '@material-ui/icons/Email';
+import './style.css';
 
 const SocialIcons = () => {
   return (
     <div className='icon-buttons'>
       <IconButton aria-label='github' href='#'>
-        <GitHubIcon />
+        <GitHubIcon id='github' />
       </IconButton>
       <IconButton aria-label='linkedin' href='#'>
-        <LinkedInIcon />
+        <LinkedInIcon id='linkedin' />
       </IconButton>
       <IconButton aria-label='email' href='#'>
-        <EmailIcon />
+        <EmailIcon id='email' />
       </IconButton>
     </div>
   );
