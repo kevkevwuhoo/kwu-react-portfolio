d496c920fd24bc623b0f4369bbc1cb9d6573a009
kwu3 <kwu3@villanova.edu>
2020-06-18 21:34:58 -0400

    Started on landing page contents, moved to folder

---
+++ src/pages/Landing/index.js
@@ -0,0 +1,36 @@
+import React from 'react';
+import './style.css';
+
+const Landing = () => {
+  return (
+    <div className='container'>
+      <div className='bg-img'></div>
+      <p>
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Commodi ut
+        doloremque earum reprehenderit, laudantium tempora aliquid vero aliquam
+        dicta sint officia ad cumque ab, vitae similique? Quam officia ipsa
+        consequuntur. Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+        Architecto ut id, porro cum debitis tempore voluptate mollitia maiores
+        iusto impedit, nobis quae ratione. Similique molestiae vitae aut atque,
+        commodi dolores? Lorem ipsum dolor sit, amet consectetur adipisicing
+        elit. Officiis, non. Beatae aperiam ullam quisquam libero consequuntur
+        minima unde alias sint dolore repellendus labore consectetur voluptates
+        dicta maiores, ducimus dolorem atque.
+      </p>
+      <p>
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Commodi ut
+        doloremque earum reprehenderit, laudantium tempora aliquid vero aliquam
+        dicta sint officia ad cumque ab, vitae similique? Quam officia ipsa
+        consequuntur. Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+        Architecto ut id, porro cum debitis tempore voluptate mollitia maiores
+        iusto impedit, nobis quae ratione. Similique molestiae vitae aut atque,
+        commodi dolores? Lorem ipsum dolor sit amet consectetur adipisicing
+        elit. Dolore consequatur quod maiores labore animi suscipit impedit
+        accusamus rem. Quod excepturi inventore, laudantium accusantium unde
+        iste fuga blanditiis eius? Repellat, fuga?
+      </p>
+    </div>
+  );
+};
+
+export default Landing;
