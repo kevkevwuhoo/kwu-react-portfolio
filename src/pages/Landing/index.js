7127aec90f60fc5fe9a8c42e47af30f0bcda9c8a
kwu3 <kwu3@villanova.edu>
2020-06-18 21:59:17 -0400

    Removed lorem text

--- src/pages/Landing/index.js
+++ src/pages/Landing/index.js
@@ -10,30 +10,6 @@ const Landing = () => {
           <h2>Full stack developer. NYC.</h2>
         </div>
       </div>
-      <p>
-        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Commodi ut
-        doloremque earum reprehenderit, laudantium tempora aliquid vero aliquam
-        dicta sint officia ad cumque ab, vitae similique? Quam officia ipsa
-        consequuntur. Lorem ipsum dolor, sit amet consectetur adipisicing elit.
-        Architecto ut id, porro cum debitis tempore voluptate mollitia maiores
-        iusto impedit, nobis quae ratione. Similique molestiae vitae aut atque,
-        commodi dolores? Lorem ipsum dolor sit, amet consectetur adipisicing
-        elit. Officiis, non. Beatae aperiam ullam quisquam libero consequuntur
-        minima unde alias sint dolore repellendus labore consectetur voluptates
-        dicta maiores, ducimus dolorem atque.
-      </p>
-      <p>
-        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Commodi ut
-        doloremque earum reprehenderit, laudantium tempora aliquid vero aliquam
-        dicta sint officia ad cumque ab, vitae similique? Quam officia ipsa
-        consequuntur. Lorem ipsum dolor, sit amet consectetur adipisicing elit.
-        Architecto ut id, porro cum debitis tempore voluptate mollitia maiores
-        iusto impedit, nobis quae ratione. Similique molestiae vitae aut atque,
-        commodi dolores? Lorem ipsum dolor sit amet consectetur adipisicing
-        elit. Dolore consequatur quod maiores labore animi suscipit impedit
-        accusamus rem. Quod excepturi inventore, laudantium accusantium unde
-        iste fuga blanditiis eius? Repellat, fuga?
-      </p>
     </div>
   );
 };
