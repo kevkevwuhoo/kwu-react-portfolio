285bade4abb9a443fdb099a1ba1f21937cae986c
kwu3 <kwu3@villanova.edu>
2020-06-23 17:58:36 -0400

    Added skills component

---
+++ src/components/Skills/index.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const Skills = () => {
+  return (
+    <div className='skills'>
+      <h2>Tech Stack</h2>
+      <ul>
+        <li id='frontend'>ReactJS, HTML, CSS, AJAX, jQuery</li>
+        <li id='backend'>NodeJS, JavaScript, ExpressJS</li>
+        <li id='other'>Webpack, Git, Jest, ES6</li>
+      </ul>
+    </div>
+  );
+};
+
+export default Skills;
