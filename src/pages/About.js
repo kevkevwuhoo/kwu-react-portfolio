1549f3b4b099ea1511553022cf17cf1f8c7fcc98
kwu3 <kwu3@villanova.edu>
2020-06-25 23:54:43 -0400

    Changed about me and removed skills from about page

--- src/pages/About.js
+++ src/pages/About.js
@@ -9,7 +9,6 @@ import {
 } from '@material-ui/core';
 import me from '../assets/images/me.jpg';
 import SubHeader from '../components/SubHeader';
-import Skills from '../components/Skills';
 
 const styles = {
   card: {
@@ -26,26 +25,33 @@ const styles = {
 function About() {
   return (
     <div className='about' id='about'>
-      <SubHeader text='About' />
+      <SubHeader text='About me' />
       <Grid container justify='center' alignItems='center' spacing={3}>
         <Grid item xs={12} md={6}>
           <Card style={styles.card}>
-            <CardHeader title='Kevin Wu' subheader='December 30, 1997' />
             <CardMedia style={styles.image} image={me} title='Kevin' />
-            <CardContent>
-              <Typography component='h1'>
-                Hello, my name is Kevin Wu.
-              </Typography>
-              <Typography variant='body2' color='textPrimary' component='p'>
-                I graduated from Villanova University with a degree in
-                Mechanical Engineering. Some of my hobbies include gaming,
-                drawing, and now coding! I am currently based in New York City.
-              </Typography>
-            </CardContent>
           </Card>
         </Grid>
+        <Grid item xs={12} md={6}>
+          <h3>I love food, puzzles, games, and most importantly, learning.</h3>
+          <br />
+          <p>
+            My drive to learn allows me to be flexible and pushes me to
+            constantly improve myself.
+          </p>
+          <br />
+          <p>
+            I started coding as a creative outlet, but I found myself lost in
+            hour-long sessions fascinated by new techniques and new
+            technologies.
+          </p>
+          <br />
+          <p>
+            My aptitude for problem solving means I am diligent and willing to
+            put in time and effort to produce functional and beautiful products.
+          </p>
+        </Grid>
       </Grid>
-      <Skills />
     </div>
   );
 }
