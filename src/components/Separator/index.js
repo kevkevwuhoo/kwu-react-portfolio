6dc46a65f1188cfcc00395ae11c075ccc224fddd
kwu3 <kwu3@villanova.edu>
2020-10-05 12:59:20 -0400

    Created separator component

---
+++ src/components/Separator/index.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import { Box, Divider } from '@material-ui/core';
+import { makeStyles } from '@material-ui/styles';
+
+const useStyles = makeStyles({
+  divider: {
+    marginTop: '40px',
+  },
+});
+
+const Separator = () => {
+  const classes = useStyles();
+  return (
+    <Box className={classes.divider}>
+      <Divider variant='middle' />
+    </Box>
+  );
+};
+
+export default Separator;
