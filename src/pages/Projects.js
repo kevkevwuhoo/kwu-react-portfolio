35bb89d56e0fbd406a3910d8641893dcbe2ea05d
kwu3 <kwu3@villanova.edu>
2020-10-05 13:00:43 -0400

    Fixed projects page structure

--- src/pages/Projects.js
+++ src/pages/Projects.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import SubHeader from '../components/SubHeader';
 import ProjectCard from '../components/ProjectCard';
-import { Grid } from '@material-ui/core';
+import { Container, Grid, Divider } from '@material-ui/core';
 import Touristee from '../assets/images/projects/touristee.png';
 import EmployeeSummary from '../assets/images/projects/employee-summary.png';
 import WeatherDashboard from '../assets/images/projects/weather-dashboard.png';
@@ -10,46 +10,49 @@ import WorkoutTracker from '../assets/images/projects/workout-tracker.png';
 
 const Projects = () => {
   return (
-    <div className='projects' id='projects'>
-      <SubHeader text='Projects' />
-      <Grid container justify='center' alignItems='center'>
-        <Grid item xs={12} sm={6} md={4}>
-          <ProjectCard
-            href='https://github.com/kevkevwuhoo/touristee'
-            src={Touristee}
-            alt='Touristee'
-          />
+    <React.Fragment>
+      <Container className='projects' id='projects'>
+        <SubHeader text='Projects' />
+        <Grid container justify='center' alignItems='center' spacing={3}>
+          <Grid item xs={12} sm={6} md={4}>
+            <ProjectCard
+              href='https://github.com/kevkevwuhoo/touristee'
+              src={Touristee}
+              alt='Touristee'
+            />
+          </Grid>
+          <Grid item xs={12} sm={6} md={4}>
+            <ProjectCard
+              href='https://github.com/kevkevwuhoo/employee-summary'
+              src={EmployeeSummary}
+              alt='Employee Summary'
+            />
+          </Grid>
+          <Grid item xs={12} sm={6} md={4}>
+            <ProjectCard
+              href='https://github.com/kevkevwuhoo/weather-dashboard'
+              src={WeatherDashboard}
+              alt='Weather Dashboard'
+            />
+          </Grid>
+          <Grid item xs={12} sm={6} md={4}>
+            <ProjectCard
+              href='https://github.com/kevkevwuhoo/work-day-scheduler'
+              src={WorkDayScheduler}
+              alt='Work Day Scheduler'
+            />
+          </Grid>
+          <Grid item xs={12} sm={6} md={4}>
+            <ProjectCard
+              href='https://github.com/kevkevwuhoo/workout-tracker'
+              src={WorkoutTracker}
+              alt='Workout Tracker'
+            />
+          </Grid>
         </Grid>
-        <Grid item xs={12} sm={6} md={4}>
-          <ProjectCard
-            href='https://github.com/kevkevwuhoo/employee-summary'
-            src={EmployeeSummary}
-            alt='Employee Summary'
-          />
-        </Grid>
-        <Grid item xs={12} sm={6} md={4}>
-          <ProjectCard
-            href='https://github.com/kevkevwuhoo/weather-dashboard'
-            src={WeatherDashboard}
-            alt='Weather Dashboard'
-          />
-        </Grid>
-        <Grid item xs={12} sm={6} md={4}>
-          <ProjectCard
-            href='https://github.com/kevkevwuhoo/work-day-scheduler'
-            src={WorkDayScheduler}
-            alt='Work Day Scheduler'
-          />
-        </Grid>
-        <Grid item xs={12} sm={6} md={4}>
-          <ProjectCard
-            href='https://github.com/kevkevwuhoo/workout-tracker'
-            src={WorkoutTracker}
-            alt='Workout Tracker'
-          />
-        </Grid>
-      </Grid>
-    </div>
+      </Container>
+      <Divider variant='middle' />
+    </React.Fragment>
   );
 };
 
