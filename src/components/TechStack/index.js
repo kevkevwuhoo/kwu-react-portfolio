78783ee325c1d69531cdd0c358f17c3691ffe542
kwu3 <kwu3@villanova.edu>
2020-10-05 13:01:33 -0400

    Updated tech stack

--- src/components/TechStack/index.js
+++ src/components/TechStack/index.js
@@ -1,15 +1,36 @@
 import React from 'react';
+import { Container, Grid } from '@material-ui/core';
+import { makeStyles } from '@material-ui/styles';
+
+const useStyles = makeStyles({
+  skills: {
+    margin: '5px',
+  },
+  label: {
+    fontStyle: 'italic',
+  },
+});
 
 const TechStack = () => {
+  const classes = useStyles();
+
   return (
-    <div className='skills'>
-      <h2>Tech Stack</h2>
-      <ul>
-        <li id='frontend'>ReactJS, HTML, CSS, AJAX, jQuery</li>
-        <li id='backend'>NodeJS, JavaScript, ExpressJS</li>
-        <li id='other'>Webpack, Git, Jest, ES6</li>
-      </ul>
-    </div>
+    <Container className='skills' className={classes.skills}>
+      <Grid container spacing={2}>
+        <Grid item xs={12} sm={4}>
+          <h2 className={classes.label}>Frontend</h2>
+          <p>ReactJS, HTML, CSS, AJAX, jQuery, D3</p>
+        </Grid>
+        <Grid item xs={12} sm={4}>
+          <h2 className={classes.label}>Backend</h2>
+          <p>NodeJS, JavaScript, ExpressJS</p>
+        </Grid>
+        <Grid item xs={12} sm={4}>
+          <h2 className={classes.label}>Other</h2>
+          <p>Webpack, Git, Jest, ES6</p>
+        </Grid>
+      </Grid>
+    </Container>
   );
 };
 
