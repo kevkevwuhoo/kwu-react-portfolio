0531284ae5012f77eedc84a398512105592b7de1
kwu3 <kwu3@villanova.edu>
2020-10-05 13:00:58 -0400

    Fixed skills page structure

--- src/pages/Skills.js
+++ src/pages/Skills.js
@@ -1,13 +1,18 @@
 import React from 'react';
+import { Container } from '@material-ui/core';
 import SubHeader from '../components/SubHeader';
 import TechStack from '../components/TechStack';
+import Separator from '../components/Separator';
 
 const Skills = () => {
   return (
-    <div className='skills' id='skills'>
-      <SubHeader text='Skills & Experience' />
-      <TechStack />
-    </div>
+    <React.Fragment>
+      <Container className='skills' id='skills'>
+        <SubHeader text='Skills & Experience' />
+        <TechStack />
+      </Container>
+      <Separator />
+    </React.Fragment>
   );
 };
 
