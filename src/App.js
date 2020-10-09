2f5e505b550414cb55c2e6737a9e11accfd8e9d1
kwu3 <kwu3@villanova.edu>
2020-10-08 22:16:14 -0400

    Removed unused imports

--- src/App.js
+++ src/App.js
@@ -1,9 +1,8 @@
 import React from 'react';
 import NavTabs from './components/NavTabs';
-import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import { BrowserRouter, Route } from 'react-router-dom';
 import Landing from './pages/Landing';
 import About from './pages/About';
-import Contact from './pages/Contact';
 import Projects from './pages/Projects';
 import Skills from './pages/Skills';
 
