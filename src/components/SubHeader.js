92ebae6261c0c9fccb5704e3990707a73ce23f46
kwu3 <kwu3@villanova.edu>
2020-10-10 00:40:48 -0400

    Changed media query styles

--- src/components/SubHeader.js
+++ src/components/SubHeader.js
@@ -1,43 +1,35 @@
 import React from 'react';
 import { Grid, Typography } from '@material-ui/core';
 import { makeStyles } from '@material-ui/core/styles';
-import useMediaQuery from '@material-ui/core/useMediaQuery';
 
 const useStyles = makeStyles({
   subheader: {
     padding: '30px',
     textTransform: 'uppercase',
+    '@media (max-width:600px)': {
+      justifyContent: 'center',
+      textAlign: 'center',
+    },
   },
   text: {
     textShadow: '2px 2px 4px #000000',
   },
   title: {
-    fontSize: '60px',
+    fontSize: '3.0rem',
     letterSpacing: '5px',
-  },
-  subheaderXS: {
-    textTransform: 'uppercase',
-  },
-  titleXS: {
-    fontSize: '45px',
-    letterSpacing: '5px',
+    '@media (max-width:600px)': {
+      fontSize: '2.6rem',
+    },
   },
 });
 
 const SubHeader = ({ text }) => {
   const classes = useStyles();
-  const matches = useMediaQuery('(min-width:600px');
 
   return (
-    <Grid
-      container
-      className={matches ? classes.subheader : classes.subheaderXS}
-    >
+    <Grid container className={classes.subheader}>
       <Grid item className={classes.text}>
-        <Typography
-          component='h3'
-          className={matches ? classes.title : classes.titleXS}
-        >
+        <Typography component='h3' className={classes.title}>
           {text}
         </Typography>
       </Grid>
